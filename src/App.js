cc3b79163a6593d80c5b7c4fa52cf5f3dc568394
Anna Filatova <filatovaa@yahoo.com>
2022-04-05 22:40:48 -0700

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createRoot } from "react-dom/client";
+import {
+  PhoneOutgoingIcon,
+  MailIcon,
+  GlobeAltIcon,
+} from "@heroicons/react/solid";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="2xl:container 2xl:mx-auto">
+      <VCard />
+    </div>
+  );
+}
+function VCard() {
+  return (
+    <div className="md:container md:mx-auto">
+      <div className="grid grid-cols-3 gap-4 justify-items-center">
+        <PhoneOutgoingIcon
+          className="fill-black hover:fill-yellow "
+          xmlns="http://www.w3.org/2000/svg"
+        />
+        <MailIcon
+          className="fill-black hover:fill-yellow "
+          xmlns="http://www.w3.org/2000/svg"
+        />
+        <GlobeAltIcon
+          className="fill-black hover:fill-yellow "
+          xmlns="http://www.w3.org/2000/svg"
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+const container = document.createElement("div");
+const root = createRoot(container);
+root.render(<App tab="home" />);
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#FD428E",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fill: {
+      black: "black",
+      pink: "pink",
+      yellow: "#FD428E",
+    },
+    container: {
+      padding: "2rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
