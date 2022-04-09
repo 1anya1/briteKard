6b442de0446a8c56e790ce3e3457f94b3b9d94b9
Anna Filatova <filatovaa@yahoo.com>
2022-04-08 17:37:49 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -10,8 +10,40 @@ export default function App() {
   return (
     <div className="2xl:container 2xl:mx-auto">
       <VCard />
+      <GetVCard />
     </div>
   );
+}
+function GetVCard() {
+  async function getCard() {
+    console.log("clicked");
+    await fetch("http://localhost:3000/vCards/Map/6250d30d4ff4877952abf798")
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (data) {
+        download("dante.VCF", data);
+        console.log(data);
+      });
+  }
+
+  function download(filename, text) {
+    var element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+    );
+    element.setAttribute("download", filename);
+
+    element.style.display = "none";
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
+  return <button onClick={getCard}>Get My Card</button>;
 }
 function VCard() {
   return (
