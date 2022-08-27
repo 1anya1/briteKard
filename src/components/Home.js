8de8fcd061014f7177bc9b07aa36c09b23884151
Anna Filatova <filatovaa@yahoo.com>
2022-08-27 00:46:46 -0700

    updated home splash

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
-/* This example requires Tailwind CSS v2.0+ */
-
 import Feature from "./FeatureSection";
 import CTA from "./CTA";
 import LogIn from "./LogIn";
 import "../images/home.svg";
+import homeSplash from "../images/home-splash.png";
 import SignUp from "./SingUp";
 // const navigation = [
 //   { name: "Product", href: "#" },
@@ -51,7 +50,7 @@ export default function Home(props) {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            src={homeSplash}
             alt=""
           />
         </div>
