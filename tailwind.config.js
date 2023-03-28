6c2d02367473ff450d485c5d77f242a6c6beb1d4
Anna Filatova <filatovaa@yahoo.com>
2023-03-27 18:19:14 -0700

    added break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
       serif: ["Merriweather", "serif"],
     },
     screens: {
-      'xs': '360px',
+      xs: '360px',
+      sm:	'640px',
+      md	:"768px",
+      lg:	"1024px",
+      xl: '1280px', 
+      "2xl":	"1536px"
 
     },
 
